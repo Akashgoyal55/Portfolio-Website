ae501d1cb311f7b024b2e5d3fd0cf5812b853c1b
Akashgoyal55 <agoyal1@sfsu.edu>
2024-08-01 18:56:45 -0700

    heading & connec update

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Typical from "react-typical";
-import ScrollService from "../../../utilities/ScrollService";
-import "./Profile.css";
 
 export default function Profile() {
   return (
@@ -9,67 +7,47 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <div className="colz-icon">
-              <a href="https://web.facebook.com/?_rdc=1&_rdr">
-                <i className="fa fa-facebook-square" />
-              </a>
-              <a href="#">
-                <i className="fa fa-google-plus-square" />
-              </a>
-              <a href="https://www.instagram.com/instructor_ehizeex/">
-                <i className="fa fa-instagram" />
-              </a>
-              <a href="https://www.youtube.com/channel/UCSSr5ZDFbilpZ592_ycoAwA">
-                <i className="fa fa-youtube-square" />
-              </a>
-              <a href="https://twitter.com/Ehiedu_baba">
-                <i className="fa fa-twitter" />
-              </a>
-            </div>
+            <a href="#">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
           </div>
+
           <div className="profile-details-name">
             <span className="primary-text">
               {" "}
-              Hello, I'M <span className="highlighted-text">Ehiedu</span>
+              Hello, I am <span className="highlighted-text">Akash</span>
             </span>
           </div>
           <div className="profile-details-role">
             <span className="primary-text">
               {" "}
-              <h1>
-                <Typical
-                  loop={Infinity}
-                  steps={[
-                    "Enthusiastic Dev 😎",
-                    1000,
-                    "Full stack Developer 💻",
-                    1000,
-                    "Mern stack Dev 📱",
-                    1000,
-                    "Cross Platform Dev 🔴",
-                    1000,
-                    "React/React Native 🌐",
-                    1000,
-                  ]}
-                />
-              </h1>
-            </span>
-            <span className="profile-role-tagline">
-              Knack of building applications with front and back end operations.
+              <h1> Full Stack Developer</h1>
+              <span className='profile-role-tagline'>
+                Enthusiastic full stack developer. 
+              </span>
             </span>
           </div>
-
-          <div className="profile-options">
-            <button className="btn primary-btn"
-            onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
-            > Hire Me </button>
-            <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
-              <button className="btn highlighted-btn">Get Resume</button>
+          <div className='profile-optins'>
+            <button className='btn primary btn'>
+                {""}
+                Hire me{" "}
+            </button>
+            <a href='AKASH RESUME.pdf' download='AKASH RESUME.pdf'>
+                <button className='btn highlighted-btn'>Get Resume</button>
             </a>
           </div>
-        </div>
-        <div className="profile-picture">
-          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
