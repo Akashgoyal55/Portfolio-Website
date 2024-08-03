1adf529e24a3bb64cce33bd00744e563186fc3c4
Akashgoyal55 <agoyal1@sfsu.edu>
2024-08-02 20:37:16 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import "./App.css";
-import Profile from "./PortfolioContainer/Home/Profile";
+import {useTypewriter, Cursor} from 'react-simple-typewriter'
+import './App.css';
 
 function App() {
+  const [typeEffect] = useTypewriter({
+    words: ['Full Stack Developer ' , 'Full Stack Developer '],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 40
+  })
   return (
-    <div className="App">
-      <Profile/>
-      
-    </div>
-  );
-}
+    <div className='App'>
+      <h1>
+        I am a 
+        <span style={{fontweight: 'bold', color: 'orange', marginLeft: '5px'}}>{typeEffect}</span>
+      </h1>
 
+    </div>
+  )
+}
 export default App;
